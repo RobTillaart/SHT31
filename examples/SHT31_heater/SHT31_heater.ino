ed38f82de48ad450090e5c8a3b9c54775f031792
Rob Tillaart <rob.tillaart@gmail.com>
2020-12-03 10:53:10 +0100

    Arduino ci (#12)
    * update CI workflow
    * fix example

--- examples/SHT31_heater/SHT31_heater.ino
+++ examples/SHT31_heater/SHT31_heater.ino
@@ -11,7 +11,7 @@
 #define SHT31_ADDRESS   0x44
 
 SHT31 sht;
-uint16_t stat;
+uint16_t status;
 
 void setup()
 {
@@ -26,15 +26,15 @@ void setup()
 
   sht.setHeatTimeout(30);  // heater timeout 30 seconds, just for demo.
 
-  stat = sht.readStatus();
-  printHeaterStatus(stat);
+  status = sht.readStatus();
+  printHeaterStatus(status);
 
   sht.heatOn();
 
   while (sht.isHeaterOn())
   {
-    stat = sht.readStatus();
-    printHeaterStatus(stat);
+    status = sht.readStatus();
+    printHeaterStatus(status);
     sht.read();
     Serial.println(sht.getTemperature());
     delay(10000);
@@ -46,15 +46,15 @@ void setup()
 void loop()
 {
   // forced switch off
-  if (stat & SHT31_STATUS_HEATER_ON) sht.heatOff();
+  if (status & SHT31_STATUS_HEATER_ON) sht.heatOff();
 }
 
 
-void printHeaterStatus(uint16_t stat)
+void printHeaterStatus(uint16_t status)
 {
   Serial.print(millis());
   Serial.print("\tHEATER: ");
-  if (stat & SHT31_STATUS_HEATER_ON)
+  if (status & SHT31_STATUS_HEATER_ON)
   {
     Serial.println("ON");
   } else {
